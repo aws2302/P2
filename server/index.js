51bb189ca231ae9bb11639649e797b8a14edaea4
Markus Rennings <markus@rennings.net>
2023-08-23 17:12:01 +0200

    Consola (Logging) hinzugefügt

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
-require('dotenv').config()
+const { createConsola } = require('consola');
+const log = createConsola({
+    fancy: true,
+    formatOptions: {
+        columns: 80,
+        colors: true,
+        date: true,
+    },
+})
+
+const dotenv_result = require('dotenv').config();
+if (dotenv_result.error) {
+    log.error("Fehler beim Laden der .env-Datei; Beende …");
+    process.exit(1);
+}
+
 const express = require('express');
 const app = express();
-const hostname = 'localhost';
-const port = 8080;
+const hostname = process.env.hostname;
+const port = parseInt(process.env.port);
 
 const cors = require('cors');
 app.use(cors());
@@ -14,5 +29,5 @@ app.get('/', (req, res) => {
 
 
 app.listen(port, hostname, () => {
-    console.log(`Server listening on ${hostname}:${port}`);
+    log.info(`Server listening on ${hostname}:${port}`);
 });
