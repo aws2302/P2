ad28d32abae28ad795ac39e08af68c19c9bb4080
Markus Rennings <markus@rennings.net>
2023-08-23 19:15:04 +0200

    reformatted

--- server/index.js
+++ server/index.js
@@ -1,33 +1,32 @@
-const { createConsola } = require('consola');
+const { createConsola } = require("consola");
 const log = createConsola({
-    fancy: true,
-    formatOptions: {
-        columns: 80,
-        colors: true,
-        date: true,
-    },
-})
+  fancy: true,
+  formatOptions: {
+    columns: 80,
+    colors: true,
+    date: true,
+  },
+});
 
-const dotenv_result = require('dotenv').config();
+const dotenv_result = require("dotenv").config();
 if (dotenv_result.error) {
-    log.error("Fehler beim Laden der .env-Datei; Beende …");
-    process.exit(1);
+  log.error("Fehler beim Laden der .env-Datei; Beende …");
+  process.exit(1);
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const hostname = process.env.hostname;
 const port = parseInt(process.env.port);
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hallo von Express!');
+app.get("/", (req, res) => {
+  res.send("Hallo von Express!");
 });
 
-
 app.listen(port, hostname, () => {
-    log.info(`Server listening on ${hostname}:${port}`);
+  log.info(`Server listening on ${hostname}:${port}`);
 });
