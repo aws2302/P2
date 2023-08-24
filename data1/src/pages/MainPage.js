67d4760d008b0832c724525708d63c86395daa8c
AndiBruehl <127749678+AndiBruehl@users.noreply.github.com>
2023-08-24 12:46:00 +0200

    added mainpage and check if user is logged in

--- data1/src/pages/MainPage.js
+++ data1/src/pages/MainPage.js
@@ -1,10 +1,40 @@
+import React, { useEffect } from "react";
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import Button from "../layout/Button";
+
 const Main = () => {
-  return  <div>
+  const auth = getAuth();
+  const navigate = useNavigate();
 
-  <h1>hallo vom Backend!</h1>
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        navigate("/");
+      }
+    });
 
-  </div>
+    return () => {
+      unsubscribe(); // Cleanup the subscription
+    };
+  }, [auth, navigate]);
 
-}
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Willkommen beim Backend!</h1>
+      <Button onClick={handleLogout}>Logout</Button>
+    </div>
+  );
+};
 
 export default Main;
