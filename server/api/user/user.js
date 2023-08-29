3dced7134ca9fdd76eb568c270f2b2dd06f0b4d1
Basti <sebastian.hufeld@docc.techstarter.de>
2023-08-29 14:44:43 +0200

    User.js wurde angepasst, damit die Weiterleitung klappt

--- server/api/user/user.js
+++ server/api/user/user.js
@@ -13,15 +13,16 @@ const router = express.Router();
 router.use(express.json());
 
 const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 // Funktion für die Registrierung und Anmeldung
-const registerAndLoginUser = async () => {
+const registerAndLoginUser = async (res) => {
   try {
     console.log("/register called");
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
     rl.question('Bitte geben Sie Ihre E-Mail-Adresse ein: ', async (email) => {
       rl.question('Bitte geben Sie Ihr Passwort ein: ', async (passwd) => {
@@ -46,10 +47,12 @@ const registerAndLoginUser = async () => {
 
             // Authentifizierung erfolgreich
             console.log("message: login success");
-            rl.close();
+            res.redirect('/user/history'); // Weiterleitung zur /history-Route nach erfolgreichem Login
           } catch (loginError) {
             // Authentifizierung nicht erfolgreich
             console.log("message: login not successful; error", loginError.message);
+            res.status(401).send('Login failed'); // Nachricht "Login failed" senden
+          } finally {
             rl.close();
           }
         } catch (error) {
@@ -64,18 +67,45 @@ const registerAndLoginUser = async () => {
   }
 };
 
-router.get('/history', (req, res) => {
+// Middleware zur Authentifizierungsüberprüfung
+const authenticateUser = (req, res, next) => {
+  const myauth = auth.getAuth();
+  const user = myauth.currentUser;
+
+  if (user) {
+    // Benutzer ist eingeloggt, führen Sie die nächste Middleware oder Route aus
+    next();
+  } else {
+    // Benutzer ist nicht eingeloggt, leiten Sie ihn zur Anmeldeseite weiter
+    if (req.originalUrl === '/history') {
+      // Nur wenn der Benutzer die /history-Route aufruft
+      res.redirect('/user/login'); // Weiterleitung zur /user/login-Route
+    } else {
+      res.status(401).send('Authentication required'); // Nachricht "Authentication required" senden
+    }
+  }
+};
+
+router.get('/history', authenticateUser, (req, res) => {
   // TODO: Alle Links des eingeloggten Users zurückgeben
   res.send('History page'); // Hier können Sie die gewünschte Antwort für die History-Seite senden
 });
 
+router.get('/history', (req, res) => {
+  res.send('Logged in'); // Hier wird "Logged in" im Browser angezeigt
+});
+
 router.get('/register', async (req, res) => {
-  registerAndLoginUser(); // Aufruf der Funktion für Registrierung und Anmeldung
-  res.send('Registration and login process initiated.');
+  registerAndLoginUser(res); // Aufruf der Funktion für Registrierung und Anmeldung und Übergabe von res
 });
 
 router.get('/login', async (req, res) => {
   console.log("/login called");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
   rl.question('Bitte geben Sie Ihre E-Mail-Adresse ein: ', async (email) => {
     rl.question('Bitte geben Sie Ihr Passwort ein: ', async (passwd) => {
@@ -87,11 +117,11 @@ router.get('/login', async (req, res) => {
 
         // Authentifizierung erfolgreich
         console.log("message: login success");
-        res.redirect('/history'); // Hier wird nach dem erfolgreichen Login zur /history Route weitergeleitet
+        res.redirect('/user/history'); // Weiterleitung zur /history-Route nach erfolgreichem Login
       } catch (error) {
         // Authentifizierung nicht erfolgreich
         console.log("message: login not successful; error", error.message);
-        res.status(401).send('Login failed'); // Hier wird die Nachricht "Login failed" an den Browser gesendet
+        res.status(401).send('Login failed'); // Nachricht "Login failed" senden
       } finally {
         rl.close();
       }
