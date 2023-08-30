1d9bfafef5c18638a5d0365968690fa94dcf99db
xKatsuoo <jasonkrimmel@songoohan.de>
2023-08-30 14:46:03 +0200

    aufgeräumt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,13 @@ import HomeIcon from '@mui/icons-material/Home';
 import { blue } from "@mui/material/colors";
 import BasicModal from "./modal";
 import ModeSwitch from "./Switch";
+import { useState } from "react";
+import { fetchSomeData } from "./Api";
 
 /* Color Presets Button */
 const primary = blue[700];
 const accent_hover = blue[900];
 
-import PersonIcon from "@mui/icons-material/Person";
-import ToggleOnOutlinedIcon from "@mui/icons-material/ToggleOnOutlined";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
-import { useState } from "react";
-import { fetchSomeData } from "./Api";
 
 
 export default function App() {
@@ -46,21 +43,6 @@ export default function App() {
           <ModeSwitch />
         </div>
         <BasicModal />
-        <div className="div-P-Icon">
-          <Button className="PersonIcon" aria-label="PersonIcon">
-            <PersonIcon className="P-Icon" />
-          </Button>
-        </div>
-        <div className="div-MS-Icon">
-          <Button className="ModeSwitch" aria-label="ModeSwitchIcon">
-            <ToggleOnOutlinedIcon className="MS-Icon" />
-          </Button>
-        </div>
-        <div className="div-H-Icon">
-          <Button className="HelpIcon" aria-label="HelpIcon">
-            <HelpOutlineOutlinedIcon className="H-Icon" />
-          </Button>
-        </div>
       </header>
       {/* Body Section */}
       <div className="body-url">
@@ -79,17 +61,15 @@ export default function App() {
               onChange={e => setUrl(e.target.value)}
             />
             <Button
+              onClick={handleSendClick}
               className="SendButton"
               variant="contained"
               endIcon={<SendIcon />}
               sx={{
                 backgroundColor: primary,
                 "&:hover": { backgroundColor: accent_hover },
-              }}
-            >
-            <Button variant="contained" endIcon={<SendIcon />} onClick={handleSendClick}>
-              Send
-            </Button>
+              }} 
+              /> 
           </div>
         </body>
       </div>
