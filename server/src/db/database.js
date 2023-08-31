37353ae4f0654e97c195e4445fa08f62f6246e20
Basti <sebastian.hufeld@docc.techstarter.de>
2023-08-31 16:03:05 +0200

    Datenbank überarbeitet

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -1,28 +1,115 @@
-const { initializeApp } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
+const { connect } = require('firefose');
+const { Schema, SchemaTypes, Query } = require('firefose');
+const { String, Object } = SchemaTypes;
+const { Model } = require('firefose');
+// Datenempfang von Markus
+// const { stats } = require('../stats');
 
-initializeApp();
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-const firestore_database = getFirestore();
+const serviceAccount = require('./p2-url-shortener-firebase-adminsdk-lfau4-b3cddbd6af.json');
 
-const saveURL = async () => {
-const URL_data = {
-  longURL: 'Los Angeles',
-  shortURL: 'CA',
+const db = connect(
+  serviceAccount, 'https://p2-url-shortener-default-rtdb.europe-west1.firebasedatabase.app'
+);
+
+const generateShortURL = () => {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let shortURL = 'short.li/';
+  for (let i = 0; i < 5; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    shortURL += characters.charAt(randomIndex);
+  }
+  return shortURL;
 };
-console.log(URL_data);
 
-// Add a new document in collection "cities" with ID 'LA'
-const res1 = await firestore_database.collection('URLs').doc('URL1').set(URL_data);
-
-const URL_Ref = firestore_database.collection('URLs').doc('URL2');
-
-const res = await URL_Ref.set({
-  capital: true
-}, { merge: true });
-
-await firestore_database.collection('URLs').doc('URL-id').set(URL_data);
-
+const askForURLData = () => {
+  readline.question('Enter the long URL: ', longURL => {
+//     readline.question('Enter the OS: ', os => {
+//       readline.question('Enter the Browser: ', browser => {
+//         readline.question('Enter the number of Clicks: ', clicks => {
+          saveURL(longURL);
+          readline.close();
+//         });
+//       });
+//     });
+  });
 };
 
-saveURL();
+const saveURL = (longURL) => {
+  const shortURL = generateShortURL();
+  const URL = new Model('Test', urlSchema);
+
+  let lastClick = new Date().toLocaleString();
+
+  // Frontend müsste dies bei sich einbauen, oder?
+  // if (parseInt(clicks) < 1) {
+  //   clicks = "0";
+  //   lastClick = "No clicks yet";
+  // }
+
+  const browser = {
+    Sonstige: 0,
+    Opera: 0,
+    Firefox: 0,
+    Chrome: 0
+  };
+
+  switch (browser) {
+    case 'Opera':
+      browser.Opera++;
+      break;
+    case 'Firefox':
+      browser.Firefox++;
+      break;
+    case 'Chrome':
+      browser.Chrome++;
+      break;
+    default:
+      browser.Sonstige++;
+  }
+
+  const data = URL.create({
+    Browser: browser,
+    // OS: os,
+    Clicks: "0",
+    shortURL,
+    longURL,
+    lastClick
+  });
+
+  const query = new Query();
+  URL.find(query).then((res) => console.warn(res));
+};
+
+const urlSchema = new Schema({
+  shortURL: {
+    type: String,
+    required: true
+  },
+  longURL: {
+    type: String,
+    required: true
+  },
+  Clicks: {
+    type: String,
+    required: true
+  },
+  // OS: {
+  //   type: String,
+  //   required: true
+  // },
+  Browser: {
+    type: Object,
+    required: true
+  },
+  lastClick: {
+    type: String,
+    required: true
+  }
+}, { timestamp: true });
+
+askForURLData();
