82993add3775ca69da92155cc1a5f10d4db33a54
xKatsuoo <jasonkrimmel@songoohan.de>
2023-09-01 16:18:47 +0200

    charts added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/components/css/index.css";
-import App from "./pages/App";
 import Routing from "./Routing";
-import Stats from "./pages/Stats";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
