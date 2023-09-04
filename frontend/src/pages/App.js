24fabb08932c1f87012a6ca1ee320f099ecd1835
Deniz <deniz.durmaz@docc.techstarter.de>
2023-09-04 14:18:22 +0200

    aufräumen

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -77,23 +77,6 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <div className="div-HS-Icon">
-          <Button
-            className="HomeIcon"
-            aria-label="HomeIcon"
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            <HomeIcon className="H-Icon" style={{ fontSize: "36px" }} />
-          </Button>
-          <Button
-            className="StatsIcon"
-            aria-label="StatsIcon"
-            onClick={() => {
-              navigate("/stats");
-            }}
-          >
-            <BarChartIcon className="S-Icon" style={{ fontSize: "36px" }} />
           <Button className="HomeIcon" aria-label="HomeIcon" onClick={handleHomeIconClick}>
             <HomeIcon className="H-Icon" style={{ fontSize: '36px' }} />
           </Button>
@@ -103,7 +86,9 @@ export default function App() {
             <BarChartIcon className="S-Icon" style={{ fontSize: '36px' }} />
           </Button>
         </div>
-        <div className="div-MS-Icon">{/* <ModeSwitch /> */}</div>
+        <div className="div-MS-Icon">
+          {/* <ModeSwitch /> */}
+        </div>
         <BasicModal />
       </header>
       {/* Body Section */}
@@ -111,17 +96,16 @@ export default function App() {
         <body className="App-Body">
           <h1>Shorty - URL Shortener</h1>
           <h3>
-            Shorty ist der URL Shortner der Gruppe 3 aus dem Techstarter Kurs
-            AWS 23-02.
+            Shorty ist der URL Shortner der Gruppe 3 aus dem Techstarter Kurs AWS 23-02.
           </h3>
           <div className="T-Field">
             <TextField
               id="outlined-basic"
               label="Enter the link here"
               variant="outlined"
-              sx={{ width: "100%" }}
+              sx={{ width: '100%' }}
               value={url} // Textfeld wird ausgelesen
-              onChange={(e) => setUrl(e.target.value)}
+              onChange={e => setUrl(e.target.value)}
             />
             <Button
               onClick={handleSendClick}
@@ -149,9 +133,6 @@ export default function App() {
               <ShortURL shortenLink={response} /> {/* Ausgabe Short-URL */}
             </div>
           )}
-          {response && passwordValue && <div style={{ margin: "10px" }}></div>}
-          {passwordValue && (
-            <Password value={passwordValue} /> // Ausgabe Password
           {response && passwordValue && (
             <div>
               <div style={{ margin: '10px' }}></div>
